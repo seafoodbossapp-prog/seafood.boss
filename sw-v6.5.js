118522208c456dc0fd5b5e9b875279a86b9a5773
seafoodbossapp-prog <seafood.boss.app@gmail.com>
2026-01-22 16:11:05 +0800

    Add files via upload

---
+++ sw-v6.5.js
@@ -0,0 +1,50 @@
+/* 檔名：sw-v6.5.js */
+const CACHE_NAME = 'seafood-boss-v6.5-layout-fix';
+
+const ASSETS_TO_CACHE = [
+    './',
+    './index.html',
+    './manifest.json',
+    './icons/icon-192.png',
+    './icons/icon-512.png',
+    'https://cdn.tailwindcss.com',
+    'https://unpkg.com/react@18/umd/react.production.min.js',
+    'https://unpkg.com/react-dom@18/umd/react-dom.production.min.js',
+    'https://unpkg.com/@babel/standalone/babel.min.js',
+    'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css'
+];
+
+self.addEventListener('install', (event) => {
+    self.skipWaiting();
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+        .then((cache) => {
+            return cache.addAll(ASSETS_TO_CACHE);
+        })
+    );
+});
+
+self.addEventListener('activate', (event) => {
+    event.waitUntil(
+        caches.keys().then((keyList) => {
+            return Promise.all(keyList.map((key) => {
+                if (key !== CACHE_NAME) {
+                    return caches.delete(key);
+                }
+            }));
+        })
+    );
+    return self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            return response || fetch(event.request).catch(() => {
+                if (event.request.headers.get('accept').includes('text/html')) {
+                    return caches.match('./index.html');
+                }
+            });
+        })
+    );
+});
